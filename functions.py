7c9950c0ef224061309eb10196b1f7d14f9257af
Maksym Bondaruk <b.max.lw@gmail.com>
2022-01-22 15:37:15 +0200

    [1] Base

---
+++ functions.py
@@ -0,0 +1,76 @@
+import math
+
+
+# [12.01.21]: takes as input any list with duplicates
+# and returns a sorted list without duplicates
+def remove_all_duplicates_from_list(self, input_list):
+    list(input_list)
+    output = []
+    duplicated = []
+    for i in input_list:
+        if list(input_list).count(i) > 1:
+            duplicated.append(i)
+            list(input_list).remove(i)
+        else:
+            output.append(i)
+            pass
+    for j in duplicated:
+        if duplicated.count(j) > 1:
+            duplicated.remove(j)
+    for x in duplicated:
+        output.append(x)
+    for h in output:
+        if output.count(h) > 1:
+            output.remove(h)
+    output.sort()
+    return output
+
+
+# [12.01.21]: Takes a number of prices and returns their total
+def get_total(self, *prices):
+    total = 0
+    for price in prices:
+        total += price
+    return total
+
+
+# [22.01.21]: Takes number and power it should
+# be raised to. Returns the powered value.
+def power(value, to_power):
+    return math.pow(value, to_power)
+
+
+# [22.01.21]: Takes a string with an email
+# and returns domain name of given email
+def get_domain(string):
+    x = str(string).find('@')
+    return string[x:]
+
+
+# [22.01.21]: Takes params for <get_evens_odds>
+def get_params():
+    start = int(input('x: '))
+    stop = int(input('y: '))
+    return [start, stop]
+
+
+# [22.01.21]: Takes an input of two integers (start/stop) and returns
+# two lists with evens and odds in range of those two integers
+def get_evens_odds(start, stop):
+    evens_list = []
+    odds_list = []
+    try:
+        for value in range(start, stop):
+            if value % 2 != 0:
+                odds_list.append(value)
+            else:
+                evens_list.append(value)
+        return f'Evens: {evens_list} \nOdds: {odds_list}'
+    except TypeError:
+        return 'Incorrect input. Enter only integer values.'
+
+
+# [22.01.21]: Takes as input any list with duplicates
+# and returns a sorted list without duplicates
+def remove_duplicates(input_list):
+    return list(set(input_list))
---
+++ main.py
@@ -0,0 +1,15 @@
+import math
+import time
+import threading as th
+
+
+x = list({2, 1, 3, 2})
+a, b, c = x[0], x[1], x[2]
+print(a, b, c)
+
+
+
+
+
+
+
