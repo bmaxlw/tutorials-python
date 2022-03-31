f4e18153909a3b45dff5e060806631a50930fcdb
Maksym Bondaruk <mbondaruk@softserveinc.com>
2022-03-31 10:56:56 +0300

    upd

--- app.py
+++ app.py
@@ -4,19 +4,3 @@ import random as rnd
 import time
 import threading as th
 
-
-def timer():
-    sec = 1
-    while True:
-        print(f'App is running for {sec} second(s)')
-        time.sleep(1)
-        sec += 1
-
-
-def get_input():
-    return input('Type something: \n')
-
-
-th.Thread(target=get_input).start()
-th.Thread(target=timer, daemon=True).start()
-
